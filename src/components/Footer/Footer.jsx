3d52a425eed1d083ce7e87c87d8190ff30dce0f2
sana <sanalayouni41.@gmail.com>
2026-02-03 01:09:41 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,74 @@
 import React from "react";
 import "./Footer.css";
+import gif from '../../assets/footer.gif'
+import { FaInstagram, FaFacebookF, FaTwitter, FaGithub } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer className="footer">
+       <hr className="line"/>
       <div className="footer-container">
+       
         {/* Left side */}
         <div className="footer-left">
-          <div className="footer-logo">B</div>
-          <p className="footer-copy">© 2025</p>
-
-          <button className="footer-btn">
-            ✨ Ask AI
-          </button>
+        <div className="footer-brand-edge">
+  <h2 className="footer-logo">Ghibli Mori</h2>
+  <p className="footer-quote">
+    “Stories are the bridge between imagination and reality.”
+  </p>
+</div>
+          <div className="footer-gif">
+        <img src={gif} alt="footer animation" />
+        </div>
         </div>
 
         {/* Right side */}
-        <div className="footer-sections">
-          {[1, 2, 3].map((_, index) => (
-            <div className="footer-section" key={index}>
-              <h4>Section</h4>
-              <ul>
-                <li>Home</li>
-                <li>Features</li>
-                <li>Pricing</li>
-                <li>FAQs</li>
-                <li>About</li>
-              </ul>
-            </div>
-          ))}
-        </div>
+<div className="footer-sections">
+
+<div className="footer-section">
+  <h4>Explore</h4>
+  <ul>
+    <li>Movies</li>
+    <li>Genres</li>
+    <li>New Releases</li>
+    <li>Top Rated</li>
+    <li>Watchlist</li>
+  </ul>
+</div>
+
+<div className="footer-section">
+  <h4>Community</h4>
+  <ul>
+    <li>My Account</li>
+    <li>Reviews</li>
+    <li>Discussions</li>
+    <li>Support</li>
+    <li>FAQs</li>
+  </ul>
+</div>
+
+<div className="footer-section">
+  <h4>Ghibli Mori</h4>
+  <ul>
+    <li>About Us</li>
+    <li>Careers</li>
+    <li>Privacy Policy</li>
+    <li>Terms of Service</li>
+    <li>Contact</li>
+  </ul>
+</div>
+
+
+</div>
+{/* SOCIAL ICONS */}
+<div className="footer-socials">
+      <FaInstagram />
+      <FaFacebookF />
+      <FaTwitter />
+      <FaGithub />
+    </div>
+   
       </div>
     </footer>
   );
